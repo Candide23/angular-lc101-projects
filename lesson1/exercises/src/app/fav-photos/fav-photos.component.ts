d4f8c2af9dc7f1eb58eaba43df44bc1bba17ea17
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-03-19 09:35:48 -0500

    I will make somes changes with css later

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  image1 = 'https://blogs.umsl.edu/news/files/2016/09/LC-818F-1.jpg';
   image2 = 'https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png';
   image3 = 'https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg';
 
